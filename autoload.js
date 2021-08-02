e1d7cd46c4d0b8f404eb58573547a526db5e3a3e
wendao3shou <80685073+wendao3shou@users.noreply.github.com>
2021-08-02 19:10:44 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -33,7 +33,7 @@ if (screen.width >= 768) {
 	]).then(() => {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			apiPath: "https://box.lautao.com/dev/live2d/",
+			apiPath: "https://proxy.wendao3shou.workers.dev/https/box.lautao.com/dev/live2d/",
 			//cdnPath: "https://cdn.jsdelivr.net/gh/wendao3shou/live2d_api/"
 		});
 	});
